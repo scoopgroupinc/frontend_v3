2245dd093e021427d15dbfd87fd21bb2fc03a65b
Clifford Owusu <cliffordowusukwadwo@gmail.com>
2023-06-20 23:01:06 +0000

    register notification on app start

--- src/navigation/ProfileNavigator.tsx
+++ src/navigation/ProfileNavigator.tsx
@@ -23,14 +23,37 @@ const ProfileNavigator = () => {
       }),
     });
 
+    const notifyMe = Notifications.addNotificationReceivedListener((notification) => {
+      console.log(notification);
+    });
+
     const responseListener = Notifications.addNotificationResponseReceivedListener(
       handleNotificationResponse
     );
 
     return () => {
       if (responseListener) Notifications.removeNotificationSubscription(responseListener);
+      if (notifyMe) Notifications.removeNotificationSubscription(notifyMe);
     };
   }, []);
+
+  // I've used to test the theory that notification sync with the backend doesn't work yet
+  // with this local notification, I've confirmed that the notification is working
+  useEffect(() => {
+    async function schedulePushNotification() {
+      await Notifications.scheduleNotificationAsync({
+        content: {
+          title: "You've got mail! 📬",
+          body: "Here is the notification body",
+          data: { data: "goes here" },
+        },
+        trigger: { seconds: 2 },
+      });
+    }
+
+    schedulePushNotification();
+  }, []);
+
   return (
     <HomeStack.Navigator
       screenOptions={{
