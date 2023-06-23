99a9ed54b07ff084f35901137658a0ccdc2fe678
Natalie Pan <natalie.pan@gmail.com>
2023-06-22 19:45:22 -0700

    turn off warning

--- src/hooks/useNotification.tsx
+++ src/hooks/useNotification.tsx
@@ -18,7 +18,7 @@ export const useNotifications = () => {
       }
       token = (await Notifications.getExpoPushTokenAsync()).data;
     } else {
-      Alert.alert("Must use physical device for Push Notifications");
+      // Alert.alert("Must use physical device for Push Notifications");
     }
 
     if (Platform.OS === "android") {
