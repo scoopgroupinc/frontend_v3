14cf5be5f0ef1e9c159838d5b4e3714db7d5618f
Clifford Owusu <cliffordowusukwadwo@gmail.com>
2023-07-03 13:25:17 +0000

    write hook to handle location

---
+++ src/hooks/useLocation.ts
@@ -0,0 +1,92 @@
+import { useMutation, useQuery } from "@apollo/client";
+import { useCallback, useEffect } from "react";
+import * as Location from "expo-location";
+import * as Device from "expo-device";
+import { Platform } from "react-native";
+import { Alert } from "native-base";
+import { SAVE_USER_LOCATION } from "../services/graphql/profile/mutations";
+import { useAppDispatch, useAppSelector } from "../store/hooks";
+import { selectUser, updateUser } from "../store/features/user/userSlice";
+import { GET_USER_LOCATION } from "../services/graphql/profile/queries";
+
+export const useLocation = () => {
+  const { user } = useAppSelector(selectUser);
+  const dispatch = useAppDispatch();
+
+  const [saveUserLocation] = useMutation(SAVE_USER_LOCATION);
+
+  const { data: userLocationData } = useQuery(GET_USER_LOCATION, {
+    variables: {
+      userId: user?.userId,
+    },
+  });
+
+  const saveUserLocationMutation = useCallback(
+    async (cordinates: any) => {
+      try {
+        const data = {
+          userId: user?.userId,
+          latitude: cordinates?.coords?.latitude.toString(),
+          longitude: cordinates?.coords?.longitude.toString(),
+        };
+
+        await saveUserLocation({
+          variables: {
+            CreateLocationInput: data,
+          },
+        });
+      } catch (err) {}
+    },
+    [saveUserLocation, user?.userId]
+  );
+
+  const getCityFromLatLong = useCallback(
+    async (cordinates: any) => {
+      const geoCity = await Location.reverseGeocodeAsync({
+        latitude: cordinates?.coords?.latitude,
+        longitude: cordinates?.coords?.longitude,
+      });
+
+      if (geoCity && geoCity[0]) {
+        const cityData = geoCity[0];
+        dispatch(
+          updateUser({
+            value: {
+              location: cityData,
+            },
+          })
+        );
+      }
+    },
+    [dispatch]
+  );
+
+  useEffect(() => {
+    (async () => {
+      if (Platform.OS === "android" && !Device.isDevice) {
+        Alert("Oops, this will not work on Snack in an Android emulator. Try it on your device!");
+        return;
+      }
+      const { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        Alert("Permission to access location was denied");
+        return;
+      }
+
+      const currentLocation = await Location.getCurrentPositionAsync({});
+
+      if (userLocationData && userLocationData.getUserLocation) {
+        const { latitude, longitude } = userLocationData.getUserLocation;
+        if (
+          latitude !== currentLocation?.coords?.latitude &&
+          longitude !== currentLocation?.coords?.longitude
+        ) {
+          saveUserLocationMutation(currentLocation);
+          getCityFromLatLong(currentLocation);
+        } else {
+          getCityFromLatLong(currentLocation);
+        }
+      }
+    })();
+  }, [saveUserLocationMutation, userLocationData, getCityFromLatLong]);
+};
--- src/navigation/ProfileNavigator.tsx
+++ src/navigation/ProfileNavigator.tsx
@@ -1,102 +1,17 @@
-import React, { useCallback, useEffect } from "react";
+import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
-import * as Device from "expo-device";
-import { Alert } from "native-base";
-import { useMutation, useQuery } from "@apollo/client";
-import * as Location from "expo-location";
-import { Platform } from "react-native";
 import { UserProfileEdit } from "../containers/home/UserProfileEdit";
 import { screenName } from "../utils/constants";
 import { UserProfileView } from "../containers/home/UserProfileView";
 import UserProfile from "../containers/home/UserProfile";
 import AppNavigator from "./AppNavigator";
 import Messages from "../containers/chat/Messages";
-import { SAVE_USER_LOCATION } from "../services/graphql/profile/mutations";
-import { selectUser, updateUser } from "../store/features/user/userSlice";
-import { useAppDispatch, useAppSelector } from "../store/hooks";
-import { GET_USER_LOCATION } from "../services/graphql/profile/queries";
+import { useLocation } from "../hooks/useLocation";
 
 const HomeStack = createStackNavigator();
 
 const ProfileNavigator = () => {
-  const { user } = useAppSelector(selectUser);
-  const dispatch = useAppDispatch();
-
-  const { data: userLocationData } = useQuery(GET_USER_LOCATION, {
-    variables: {
-      userId: user?.userId,
-    },
-  });
-
-  const [saveUserLocation] = useMutation(SAVE_USER_LOCATION);
-  const saveUserLocationMutation = useCallback(
-    async (cordinates: any) => {
-      try {
-        const data = {
-          userId: user?.userId,
-          latitude: cordinates?.coords?.latitude.toString(),
-          longitude: cordinates?.coords?.longitude.toString(),
-        };
-
-        await saveUserLocation({
-          variables: {
-            CreateLocationInput: data,
-          },
-        });
-      } catch (err) {
-        // console.error(err);
-      }
-    },
-    [saveUserLocation, user?.userId]
-  );
-
-  const getCityFromLatLong = useCallback(async (cordinates: any) => {
-    const geoCity = await Location.reverseGeocodeAsync({
-      latitude: cordinates?.coords?.latitude,
-      longitude: cordinates?.coords?.longitude,
-    });
-
-    if (geoCity && geoCity[0]) {
-      const cityData = geoCity[0];
-      dispatch(
-        updateUser({
-          value: {
-            location: cityData,
-          },
-        })
-      );
-    }
-  }, []);
-
-  useEffect(() => {
-    (async () => {
-      if (Platform.OS === "android" && !Device.isDevice) {
-        Alert("Oops, this will not work on Snack in an Android emulator. Try it on your device!");
-        return;
-      }
-      const { status } = await Location.requestForegroundPermissionsAsync();
-      if (status !== "granted") {
-        Alert("Permission to access location was denied");
-        return;
-      }
-
-      const currentLocation = await Location.getCurrentPositionAsync({});
-
-      if (userLocationData && userLocationData.getUserLocation) {
-        const { latitude, longitude } = userLocationData.getUserLocation;
-        if (
-          latitude !== currentLocation?.coords?.latitude &&
-          longitude !== currentLocation?.coords?.longitude
-        ) {
-          saveUserLocationMutation(currentLocation);
-          getCityFromLatLong(currentLocation);
-        } else {
-          getCityFromLatLong(currentLocation);
-        }
-      }
-    })();
-  }, [saveUserLocationMutation, userLocationData, getCityFromLatLong]);
-
+  useLocation();
   return (
     <HomeStack.Navigator
       screenOptions={{
