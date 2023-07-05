2b5a3037631280c0c1ae5216a1f976d6cd119457
Natalie Pan <natalie.pan@gmail.com>
2023-07-05 16:05:54 -0700

    SCoop-106 floating nav component

---
+++ src/components/molecule/FloatingNav/index.tsx
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import { View, TouchableOpacity } from "react-native";
+import { Colors } from "../../../utils";
+import { styles } from "./styles";
+import { FloatingNavItemList } from "./types";
+import { FloatingNavButton } from "./FloatingNavButton";
+
+
+export const FloatingNav = ({ items }: FloatingNavItemList) => {
+  const gradient = [Colors.RUST, Colors.RED, Colors.TEAL];
+  const [isPressedId, setIsPressedId] = useState("");
+
+  return (
+    <View style={styles.container}>
+      {items.map(({ id, icon, onPress, isSelected }) => (
+        <TouchableOpacity
+          onPressIn={() => setIsPressedId(id)}
+          onPressOut={() => setIsPressedId("")}
+          onPress={onPress}
+        >
+          <FloatingNavButton icon={icon} focused={id === isPressedId || isSelected} />
+        </TouchableOpacity>
+      ))}
+    </View>
+  );
+};
