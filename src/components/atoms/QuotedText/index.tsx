f1460597683c876e498066ac6f5d1acaa7e2b9ad
Natalie Pan <natalie.pan@gmail.com>
2023-07-07 19:44:35 -0700

    styling quote

--- src/components/atoms/QuotedText/index.tsx
+++ src/components/atoms/QuotedText/index.tsx
@@ -1,14 +1,11 @@
 import React from "react";
-import { Image, Text, View } from "react-native";
+import { Text, View } from "react-native";
 import { styles } from "./style";
 import { QuotedTextType } from "./types";
 
 export const QuotedText = ({ title, text }: QuotedTextType) => (
   <View>
-    <Image
-      source={require("../../../assets/images/quote.png")}
-      style={{ position: "absolute", left: -50 }}
-    />
+    <Text style={styles.quote}>"</Text>
     <View>
       <View style={styles.textContainer}>
         <Text style={styles.title}>{title}</Text>
