8bbf2cbfc73d5de38e1bb93459eac3d79107dd45
Natalie Pan <natalie.pan@gmail.com>
2023-07-19 00:47:29 -0700

    undo remove persistant store

--- src/store/index.ts
+++ src/store/index.ts
@@ -21,7 +21,7 @@ const rootReducer = combineReducers({
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: rootReducer,
+  reducer: persistedReducer,
   middleware: getDefaultMiddleware({
     serializableCheck: false,
     // {
