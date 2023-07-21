562cc8e006fb3c5ae685dbb6d757cbf71478e6a3
Clifford Owusu <cliffordowusukwadwo@gmail.com>
2023-07-21 13:38:10 +0000

    create component for social login buttons

---
+++ src/components/molecule/SocialLogins/styles.ts
@@ -0,0 +1,10 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  // btnContainer: {
+  //   alignItems: "center",
+  //   flex: 1,
+  //   justifyContent: "flex-end",
+  //   width: "100%",
+  // },
+});
