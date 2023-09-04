eecead02eeb7468ca5be8eb3053fd64075515f53
Clifford Owusu <cliffordowusukwadwo@gmail.com>
2023-09-04 10:04:19 -0400

    change query to mutation

--- src/services/graphql/user-link/mutations.ts
+++ src/services/graphql/user-link/mutations.ts
@@ -36,3 +36,53 @@ export const ACTIVATE_PROFILE_LINK = gql`
     }
   }
 `;
+
+export const GET_USER_PROFILE_BY_LINK_ID = gql`
+  mutation GetUserProfileByLinkId($id: String!) {
+    getUserProfileByLinkId(id: $id) {
+      userId
+      createdAt
+      profilePhoto
+      displayName
+      birthday
+      height
+      gender
+      locationId
+      jobTitle
+      company
+      homeTown
+      school
+      promptIds
+      prompts {
+        id
+        createdAt
+        userId
+        promptId
+        answer
+      }
+      tags {
+        id
+        userId
+        tagType
+        visible
+        userTags {
+          id
+          userId
+          tagName
+          tagType
+        }
+      }
+      visuals {
+        id
+        createdAt
+        userId
+        videoOrPhoto
+        blobName
+        visualPromptId
+        deletedAt
+        description
+        isVisible
+      }
+    }
+  }
+`;
