afbe081a0145d40a1742365c289780e0d65376df
Clifford Owusu <cliffordowusukwadwo@gmail.com>
2023-11-20 12:00:27 -0500

    hide bottom navigation

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -17,76 +17,77 @@ const AppNavigator = () => {
   const isOnboarded = useAppSelector(selectUserIsOnboarded);
 
   return isOnboarded ? (
-    <AppTabStack.Navigator
-      initialRouteName={screenName.PROFILE_HOME}
-      screenOptions={{
-        headerShown: false,
-        tabBarShowLabel: false,
-        tabBarStyle: {
-          position: "absolute",
-          bottom: 25,
-          left: "20%",
-          right: "20%",
-          backgroundColor: Colors.WHITE,
-          borderRadius: 100,
-          height: 42,
-          display: "flex",
-          flexWrap: "wrap",
-          width: "auto",
-          shadowColor: "#000",
-          shadowOffset: {
-            // for iOS
-            width: 0,
-            height: 3,
-          },
-          elevation: 5, // for Android
-          shadowOpacity: 0.25, // for iOS
-          shadowRadius: 3.5, // for iOS
-        },
-      }}
-    >
-      <AppTabStack.Screen
-        name={screenName.VOTE}
-        component={VoteNavigator}
-        options={{
-          tabBarItemStyle: {
-            height: 40,
-          },
-          tabBarIcon: ({ focused }) => {
-            const icon = "thumbs-up";
-            return <FloatingNavButton icon={icon} focused={focused} />;
-          },
-        }}
-      />
-      <AppTabStack.Screen
-        name={screenName.PROFILE_HOME}
-        component={Home}
-        options={{
-          tabBarItemStyle: {
-            height: 40,
-          },
-          tabBarIcon: ({ focused }) => {
-            const icon = "person-outline";
-            return <FloatingNavButton icon={icon} focused={focused} />;
-          },
-        }}
-      />
+    // <AppTabStack.Navigator
+    //   initialRouteName={screenName.PROFILE_HOME}
+    //   screenOptions={{
+    //     headerShown: false,
+    //     tabBarShowLabel: false,
+    //     tabBarStyle: {
+    //       position: "absolute",
+    //       bottom: 25,
+    //       left: "20%",
+    //       right: "20%",
+    //       backgroundColor: Colors.WHITE,
+    //       borderRadius: 100,
+    //       height: 42,
+    //       display: "flex",
+    //       flexWrap: "wrap",
+    //       width: "auto",
+    //       shadowColor: "#000",
+    //       shadowOffset: {
+    //         // for iOS
+    //         width: 0,
+    //         height: 3,
+    //       },
+    //       elevation: 5, // for Android
+    //       shadowOpacity: 0.25, // for iOS
+    //       shadowRadius: 3.5, // for iOS
+    //     },
+    //   }}
+    // >
+    //   <AppTabStack.Screen
+    //     name={screenName.VOTE}
+    //     component={VoteNavigator}
+    //     options={{
+    //       tabBarItemStyle: {
+    //         height: 40,
+    //       },
+    //       tabBarIcon: ({ focused }) => {
+    //         const icon = "thumbs-up";
+    //         return <FloatingNavButton icon={icon} focused={focused} />;
+    //       },
+    //     }}
+    //   />
+    //   <AppTabStack.Screen
+    //     name={screenName.PROFILE_HOME}
+    //     component={Home}
+    //     options={{
+    //       tabBarItemStyle: {
+    //         height: 40,
+    //       },
+    //       tabBarIcon: ({ focused }) => {
+    //         const icon = "person-outline";
+    //         return <FloatingNavButton icon={icon} focused={focused} />;
+    //       },
+    //     }}
+    //   />
 
-      <AppTabStack.Screen
-        name={screenName.CHAT_NAVIGATOR}
-        component={ChatNavigator}
-        options={{
-          title: "Conversations",
-          tabBarItemStyle: {
-            height: 40,
-          },
-          tabBarIcon: ({ focused }) => {
-            const icon = "chatbox-ellipses-outline";
-            return <FloatingNavButton icon={icon} focused={focused} />;
-          },
-        }}
-      />
-    </AppTabStack.Navigator>
+    //   <AppTabStack.Screen
+    //     name={screenName.CHAT_NAVIGATOR}
+    //     component={ChatNavigator}
+    //     options={{
+    //       title: "Conversations",
+    //       tabBarItemStyle: {
+    //         height: 40,
+    //       },
+    //       tabBarIcon: ({ focused }) => {
+    //         const icon = "chatbox-ellipses-outline";
+    //         return <FloatingNavButton icon={icon} focused={focused} />;
+    //       },
+    //     }}
+    //   />
+    // </AppTabStack.Navigator>
+    <Home />
   ) : (
     <OnboardNavigator />
   );
